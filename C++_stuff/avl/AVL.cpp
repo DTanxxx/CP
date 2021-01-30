ea6cc33c9d21c810c5a6468bef3eaaf9e9a3c247
David Tan <>
2021-01-30 15:38:01 +0800

    Removed everything

--- C++_stuff/avl/AVL.cpp
+++
@@ -1,5 +0,0 @@
-
-// This file has been intentionally left blank after some reorganizing since
-// the lectures were originally recorded. Please refer to the "AVL.hpp" file
-// in the same directory.
-
--- Codef/Medium_Problems/names.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "KABIR" << std::endl;
-    return 0;
-}
